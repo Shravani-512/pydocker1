812e96762f8899fcfef5c1e9a9c675acbe5b21fd
Shravani-512 <annamanenishravani01@gmail.com>
2024-12-12 16:19:52 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,4 @@
 # fibonacci.py
-# fibonacci.py
 
 def fibonacci(n):
     fib_sequence = [0, 1]
@@ -12,4 +11,5 @@ n = 10  # You can change this number to generate more or fewer Fibonacci numbers
 
 # Generate and print the Fibonacci sequence
 print(fibonacci(n))
+
 
